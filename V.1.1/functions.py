78137f21bfc87166e0b6e2fff01a4e88a2358568
Dany Al-Moghrabi Darghouth <dany.al-moghrabi@etu.unige.ch>
2022-08-18 22:27:56 +0200

    MODIFY V.1.1

--- V.1.1/functions.py
+++ V.1.1/functions.py
@@ -1,7 +1,10 @@
+import os
 from pytube import YouTube
 
 
+
 def urlCheck(url):
+# Checks if url has the YT video url header format
     toCheck = url
     normalUrlHeader ="https://www.youtube.com/watch?"
     headerToCheck = url[0:len(normalUrlHeader)]
@@ -12,7 +15,9 @@ def urlCheck(url):
          
     return check
 
-def downloadVideo():
+def downloadPrep():
+# Gets Url from user and Checks if Youtube video is reachable and returns URL 
+   
     while True : 
         url = input("Paste Video URL:")
         if(urlCheck(url)!=0):
@@ -21,15 +26,58 @@ def downloadVideo():
 
         try:
             url = YouTube(url)
-            break
+            return url
         except :
+            #If video is not reachable try loop back and ask for new URL
             print("The video Could not be found, please try again with a different URL.")
             continue
 
+def downloadVideo():
+
+    url = downloadPrep()
     try:    
         video = url.streams.get_highest_resolution().download()
     except:
         print("ERROR downloading the Video")
+
+def downloadAudio():
+    url = downloadPrep()
+
+    try:    
+        video = url.streams.filter(only_audio=True)
+        video[0].download()
+        old = url.title+".mp4"
+        new = url.title+".mp3"
+        os.rename(old, new)
+    except:
+        print("ERROR downloading the Audio file")
+
+
+# Menu functions
+
+def menu():
+    while True :
+        print("\n1. Donwload a Full Video in .mp4 format")
+        print("2. Only Get Audio File from a video in .mp3 format ")
+        print("3. Close")
+
+        choice = int(input("Please type your choice: \n"))
+    
+        if choice == 1:
+            downloadVideo()
+            continue
+        elif choice == 2:
+            downloadAudio()
+            continue
+        elif choice == 3:
+            print("Thank you for using YoutubeDownloader !")
+            break
+        else: 
+            print(choice + " is not a Valide input, please try again.\n")
+            continue
+            
+            
+    
 
 
 
