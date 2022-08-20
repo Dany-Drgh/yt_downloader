05c7be8ffd69c11a1ec5bedbe5c257e3c03ec966
Dany Al-Moghrabi Darghouth <dany.al-moghrabi@etu.unige.ch>
2022-08-21 00:22:31 +0200

    Fixing version name in menu

--- V.1.1/main.py
+++ V.1.1/main.py
@@ -3,7 +3,7 @@ from functions import *
 from string import *
 
 tprint("Youtube Downloader")
-print("Welcome to Youtube Downloader V.1.0 \n")
+print("Welcome to Youtube Downloader V.1.1 \n")
 menu()
 
     
